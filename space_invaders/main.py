1e9746c8d8ddabeb59a6cd1f42e9ff7710b2e1c1
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2022-09-05 13:41:07 -0300

    :rocket: initial commit

---
+++ space_invaders/main.py
@@ -0,0 +1,88 @@
+from typing import List
+
+
+class Position:
+    x: int
+    y: int
+
+
+class CollisionObserver:
+    observers: list
+
+    def register(self, observer):
+        ...
+
+    def notify(self, player):
+        for observer in self.observers:
+            observer.check_colision(player)
+
+
+class Score:
+    _score: int
+
+
+class Bullet:
+    position: Position
+
+
+class CollisionChecker:
+    def check_collision(self, player):
+        ...
+
+
+class Invader:
+    position: Position
+
+
+class Invaders(CollisionChecker):
+    _invaders: List[Invader]
+
+
+class Asteroid:
+    position: Position
+
+
+class Asteroids(CollisionChecker):
+    _asteroids: List[Asteroid]
+
+
+class SpaceShip:
+    position: Position
+    bullets: List[Bullet]
+
+    def move_right(self):
+        ...
+
+    def move_left(self):
+        ...
+
+    def shoot(self):
+        ...
+
+
+class Game:
+    width: int
+    height: int
+    position: Position
+    score: Score
+    invaders: Invaders
+    asteroids: Asteroids
+    space_ship: SpaceShip
+    observer: CollisionObserver
+
+    def __init__(self):
+        self.observer.register(self.asteroids)
+        self.observer.register(self.invaders)
+        self.observer.register(self.score)
+
+    def run(self):
+        while True:
+            command = self.get_command()  # type: ignore
+            if command == "LEFT":
+                self.space_ship.move_left()
+            elif command == "RIGHT":
+                self.space_ship.move_right()
+            elif command == "SHOOT":
+                self.space_ship.shoot()
+            else:
+                break
